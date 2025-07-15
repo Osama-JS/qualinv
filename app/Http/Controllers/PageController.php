6e7a1054692c0a0821690048396e5476d400cc33
Osama-JS <osama.samomy@gmail.com>
2025-07-15 08:17:59 +0300

    fix show pages method

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\Page;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class PageController extends Controller
@@ -12,25 +13,33 @@ class PageController extends Controller
      * Display a dynamic page by slug
      */
     public function show(string $slug): View
-    {
-        $locale = app()->getLocale();
+{
+    $locale = app()->getLocale();
+    $driver = DB::getDriverName(); // 'mysql' or 'pgsql'
 
-        // Find page by slug in current locale
-        $page = Page::active()
-            ->where(function ($query) use ($slug, $locale) {
+    $page = Page::active()
+        ->where(function ($query) use ($slug, $locale, $driver) {
+            if ($driver === 'pgsql') {
+                // PostgreSQL uses ->> operator
+                $query->whereRaw("slug->>'{$locale}' = ?", [$slug])
+                      ->orWhereRaw("slug->>'en' = ?", [$slug])
+                      ->orWhereRaw("slug->>'ar' = ?", [$slug]);
+            } else {
+                // MySQL uses JSON_EXTRACT
                 $query->whereRaw("JSON_EXTRACT(slug, '$.{$locale}') = ?", [$slug])
                       ->orWhereRaw("JSON_EXTRACT(slug, '$.en') = ?", [$slug])
                       ->orWhereRaw("JSON_EXTRACT(slug, '$.ar') = ?", [$slug]);
-            })
-            ->firstOrFail();
+            }
+        })
+        ->firstOrFail();
 
-        // Get page data for SEO
-        $pageTitle = $page->getLocalizedMetaTitle($locale);
-        $pageDescription = $page->getLocalizedMetaDescription($locale);
-        $pageKeywords = $page->getLocalizedMetaKeywords($locale);
-        $company = Company::first();
+    // Get page data for SEO
+    $pageTitle = $page->getLocalizedMetaTitle($locale);
+    $pageDescription = $page->getLocalizedMetaDescription($locale);
+    $pageKeywords = $page->getLocalizedMetaKeywords($locale);
+    $company = Company::first();
 
+    return view('page.show', compact('page', 'pageTitle', 'pageDescription', 'pageKeywords', 'company'));
+}
 
-        return view('page.show', compact('page', 'pageTitle', 'pageDescription', 'pageKeywords','company'));
-    }
 }
